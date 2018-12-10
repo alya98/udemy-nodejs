20caf9e7a9af67133b35dee2032c3c8da7d07f0f
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-10 15:48:30 +0300

    Add server logs into fule

--- web-server/server.js
+++ web-server/server.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
+hbs.registerPartials(__dirname+ '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log+'\n', err => {
+    if (err) console.log('unable to append a file')
+  })
+  next();
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
 app.use(express.static(__dirname+'/public'));
 
-app.get('/', (request, response) => {
-  response.send('<h1>hello i am alino4ka</h1>')
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-app.get('/about', (req,res) => {
-  res.send('about page')
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my Home page!',
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  })
 })
 
 app.get('/bad', (req,res) => {
